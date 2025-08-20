4966ee953b624b2efe8bc940e8fe6d8e7f563fe8
thomasO82 <oliver-thomas@live.fr>
2025-08-20 14:45:11 +0200

    correction td 2

---
+++ app/assets/burger.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('mobile-menu-button');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
--- app/src/Controller/TestController.php
+++ app/src/Controller/TestController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class TestController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
     }
 }
